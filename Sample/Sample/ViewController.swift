26a118ae6c0f718a26944979870f8c71590b45d4
Matheus Bispo <matheus.filipe.bispo@accenture.com>
2019-10-02 09:16:12 -0300

    Add sample initial projects

---
+++ Sample/Sample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Sample
+//
+//  Created by matheus.filipe.bispo on 02/10/19.
+//  Copyright © 2019 BootcampCS-Set2019. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
