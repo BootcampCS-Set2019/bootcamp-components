6ccdcf3a585c2a9f820f2ee32cb6600f58c6d7ff
Bruno <bruno.c.carvalho@accenture.com>
2019-10-08 17:36:33 -0300

    Add search bar to components

--- Sample/ViewController.swift
+++ Sample/ViewController.swift
@@ -7,12 +7,39 @@
 //
 
 import UIKit
+import Components
+import Resources
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        self.view.backgroundColor = .black
+
+        let searchBar = MagicDesignSystem.SearchBar.searchCards.uiSearchBar(
+            frame: CGRect(x: 0, y: 100, width: self.view.frame.width, height: 40),
+            placeholder: "Search for cards")
+        self.view.addSubview(searchBar)
+        searchBar.delegate = self
+        print()
     }
 
+}
+
+extension ViewController: UISearchBarDelegate {
+    func searchBarTextDidBeginEditing(_ searchBar: UISearchBar) {
+        searchBar.setShowsCancelButton(true, animated: true)
+    }
+
+    func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
+        searchBar.text = ""
+        searchBar.setShowsCancelButton(false, animated: true)
+        searchBar.resignFirstResponder()
+    }
+
+    func searchBarShouldEndEditing(_ searchBar: UISearchBar) -> Bool {
+        searchBar.setShowsCancelButton(false, animated: true)
+        return true
+    }
 }
